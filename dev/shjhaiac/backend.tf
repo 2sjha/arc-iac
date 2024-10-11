47267b64eb1c1d40ffe8f2ffc6fd3545b6563010
Shubham Shekhar Jha <shjha@microsoft.com>
2024-10-11 13:42:20 -0700

    test12

--- dev/shjhaiac/backend.tf
+++ dev/shjhaiac/backend.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name  = "shjha-arciac-rg"
     storage_account_name = "shjhaarciac"
     container_name       = "shjhaarciac"
-    key                  = "sample.tfstate"
+    key                  = "shjhaiac.tfstate"
     use_azuread_auth     = true
     subscription_id      = "cff3e2b4-e15d-4d1d-9345-e807c566dc1c"
   }
